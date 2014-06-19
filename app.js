563b7f1701b61f13c8c1b8ba3c310a58c59fc309
Bob Nisco <BobNisco@gmail.com>
2014-06-19 19:51:16 -0400

    Derp

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var querystring = require('querystring');
 
 var client_id = '0dc431a2682b462e93cd00fbf8295447'; // Your client id
 var client_secret = '3c29e4ee31b242758532b7c12cffe4a5'; // Your client secret
-var redirect_uri = 'http://spotlistr.heroku.com/callback'; // Your redirect uri
+var redirect_uri = 'http://spotlistr.herokuapp.com/callback'; // Your redirect uri
 
 // Change the redirect URI if we are developing
 // To set NODE_ENV in Windows: SET NODE_ENV=development
