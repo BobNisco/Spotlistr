7e044cd8262bf9edb817e80f9e1cf2f7ef0e5ec2
Bob Nisco <BobNisco@gmail.com>
2014-07-11 22:37:46 -0400

    Bower update

--- public/bower_components/angulartics/src/angulartics-kissmetrics.js
+++ public/bower_components/angulartics/src/angulartics-kissmetrics.js
@@ -1,5 +1,5 @@
 /**
- * @license Angulartics v0.15.19
+ * @license Angulartics v0.15.20
  * (c) 2013 Luis Farzati http://luisfarzati.github.io/angulartics
  * License: MIT
  */
@@ -12,21 +12,21 @@
  * Enables analytics support for KISSmetrics (http://kissmetrics.com)
  */
 angular.module('angulartics.kissmetrics', ['angulartics'])
-.config(['$analyticsProvider', '$window', function ($analyticsProvider, $window) {
+.config(['$analyticsProvider', function ($analyticsProvider) {
 
   // KM already supports buffered invocations so we don't need
   // to wrap these inside angulartics.waitForVendorApi
 
   // Creates the _kqm array if it doesn't exist already
   // Useful if you want to load angulartics before kissmetrics
-  $window._kmq = _kmq || [];
+  window._kmq = _kmq || [];
 
   $analyticsProvider.registerPageTrack(function (path) {
-    $window._kmq.push(['record', 'Pageview', { 'Page': path }]);
+    window._kmq.push(['record', 'Pageview', { 'Page': path }]);
   });
 
   $analyticsProvider.registerEventTrack(function (action, properties) {
-    $window._kmq.push(['record', action, properties]);
+    window._kmq.push(['record', action, properties]);
   });
 
 }]);
