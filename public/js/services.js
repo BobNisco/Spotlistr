cbc83d4e723c007173664d48459e82eaf815eafe
Bob Nisco <BobNisco@gmail.com>
2014-07-13 17:01:46 -0400

    Bump up that version number

--- public/js/services.js
+++ public/js/services.js
@@ -2,7 +2,7 @@
 
 /* Services */
 angular.module('spotlistr.services', [])
-	.value('version', '1.2')
+	.value('version', '1.5')
 	.factory('UserFactory', function($http, $rootScope) {
 		return {
 			currentUser: function() {
