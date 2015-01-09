8bcef1352f30f8560f66212a350c68bdeff0d953
Bob Nisco <BobNisco@gmail.com>
2015-01-08 23:34:55 -0800

    Bump version to 1.6.1

--- public/js/services.js
+++ public/js/services.js
@@ -2,7 +2,7 @@
 
 /* Services */
 angular.module('spotlistr.services', [])
-	.value('version', '1.6.0')
+	.value('version', '1.6.1')
 	.factory('UserFactory', function($http, $rootScope) {
 		return {
 			currentUser: function() {
