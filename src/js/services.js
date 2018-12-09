e521f1d611db000440065b9a94326fd00026fc4a
Bob Nisco <1316246+BobNisco@users.noreply.github.com>
2018-12-09 13:34:20 -0800

    v1.14.13

--- src/js/services.js
+++ src/js/services.js
@@ -2,7 +2,7 @@
 
 angular
   .module('spotlistr.services', [])
-  .value('version', '1.14.12')
+  .value('version', '1.14.13')
   .factory('UserFactory', function($http, $rootScope) {
     return {
       reauthAttempts: 0,
